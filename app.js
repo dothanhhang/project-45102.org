ecd6b7ebb7632e5a6741a3c1459cbb584c50610c
dothanhhang <phohanoi234@yahoo.com>
2018-03-07 17:03:44 +0700

    Buoc khoi dau

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let express = require('express')
+var bodyParser = require('body-parser');
 
 let app = express()
 
@@ -8,9 +9,19 @@ app.set('port',8000)
 app.set('view engine','ejs')
 
 app.use(express.static(`${__dirname}/public`))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+// Đây là thủ thuật để đưa đường dẫn vào trong thư mục bất kỳ
 
 app.get('/',(req, res) => {
     res.status(200).render('index')
+})
+
+app.get('/sendMail',  (req,res)=> {
+    console.log('get request for /')
+    res.send('Hellooooooooooo')
 })
 
 app.listen(app.get('port'),() =>{
